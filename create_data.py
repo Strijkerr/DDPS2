3d4d1cf454ef6be52a603eb0da1437dbf70b63ea
Jonathan S <jonastrijker@gmail.com>
2022-12-13 22:59:07 +0100

    bugfix in createdata

--- create_data.py
+++ create_data.py
@@ -6,6 +6,7 @@ seed = 0
 np.random.seed(seed)
 path = os.getcwd()
 sequence_length = 100000000
-sequence = np.random.randint(0, 9, sequence_length)
+sequence = np.random.randint(0, 10, sequence_length)
 count = collections.Counter(sequence)
+print(count)
 np.save(os.path.join(path, 'sequence'), sequence)
