2048301a8a70623878b29e178c677f903097a7ab
Jonathan S <jonastrijker@gmail.com>
2022-12-13 02:07:45 +0100

    tried some things

--- main.py
+++ main.py
@@ -1,19 +1,51 @@
 import os
 import numpy as np
+import argparse
+import socket
+import re
 
-workers = 5
-reducers = 0
+nodelist = ['node102']
 
-path = os.getcwd()
-sequence = np.load(os.path.join(path, 'sequence.npy'))
-splits = np.split(sequence,workers)
+def check_ssh(server_ip, port=22):
+    try:
+        test_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        test_socket.connect((server_ip, port))
+    except Exception:
+        return False
+    else:
+        test_socket.close()
+    return True
 
-# scp /path/to/file username@a:/path/to/destination
+def checkInput (string) :
+    nodes = args.workers.strip().split(',')
+    # TODO implement regex
+    # pattern
+    # matches_format = all()
+    return nodes
+    
+def splitInput (filename, workers) :
+    path = os.getcwd()
+    worker_count = len(workers)
+    sequence = np.load(os.path.join(path, filename))
+    return np.split(sequence,worker_count)
+
+# Parsing command line arguments
+argparser = argparse.ArgumentParser()
+argparser.add_argument("--workers", help="E.g., node102,node103,node104 ")
+argparser.add_argument("--input_file", default= 'sequence.npy', help="E.g., node102,node103,node104 ")
+args = argparser.parse_args()
+
+if args.workers is None:
+    raise ValueError('No workers specified.')
+
+nodes = checkInput(args.workers)
+master = nodes[0]
+workers = nodes[1:]
+
+splits = splitInput (args.input_file, workers)
 print(splits)
-
-
-
-
+print(f"Can connect to {master}: {check_ssh(master)}")
+# scp /path/to/file username@a:/path/to/destination
 
 
 pid = os.fork()
