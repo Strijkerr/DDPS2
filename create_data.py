74d49d718d562738fc98b0714bb2b11b9b08db9e
Jonathan S <jonastrijker@gmail.com>
2022-12-13 22:03:58 +0100

    sequence is now 800mb

--- create_data.py
+++ create_data.py
@@ -5,7 +5,7 @@ import os
 seed = 0 
 np.random.seed(seed)
 path = os.getcwd()
-sequence_length = 1000000
+sequence_length = 100000000
 sequence = np.random.randint(0, 9, sequence_length)
 count = collections.Counter(sequence)
 np.save(os.path.join(path, 'sequence'), sequence)
--- main.py
+++ main.py
@@ -93,21 +93,20 @@ for i, split in enumerate(file_splits) :
 for worker, file in zip(workers,os.listdir(dirName)):
     copyShards (worker, file)
 
+pid = os.fork()
 
-# pid = os.fork()
-
-# # The parent process (master node)
-# if pid > 0 :
-#     print(f"Parent: {pid}")
-#     process = subprocess.Popen(f"ssh {master} python3 ~/DDPS2/helloworld.py", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-#     stdout, stder = process.communicate() # Blocking
-#     print(pid,stdout)
-# # The created child process (worker nodes)
-# else :
-#     for worker in workers:
-#         pid = os.fork()
-#         if pid:
-#             print(f"Child: {pid}")
-#             process = subprocess.Popen(f"ssh {worker} python3 ~/DDPS2/helloworld.py", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-#         else:
-#             os._exit(0)
+# The parent process (master node)
+if pid > 0 :
+    print(f"Parent: {pid}")
+    process = subprocess.Popen(f"ssh {master} python3 ~/DDPS2/helloworld.py", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stder = process.communicate() # Blocking
+    print(pid,stdout)
+# The created child process (worker nodes)
+else :
+    for worker in workers:
+        pid = os.fork()
+        if pid:
+            print(f"Child: {pid}")
+            process = subprocess.Popen(f"ssh {worker} python3 ~/DDPS2/helloworld.py", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        else:
+            os._exit(0)
