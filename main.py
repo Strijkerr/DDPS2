eb347d504d1e93d80b531e52c612e683106d8881
Jonathan S <jonastrijker@gmail.com>
2022-12-13 02:26:40 +0100

    comments

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ import os
 import numpy as np
 import argparse
 import socket
-import re
 
-nodelist = ['node102']
+# Parsing command line arguments
+def command_line_arguments () :
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("--nodes", help="E.g., node102,node103,node104 ")
+    argparser.add_argument("--input_file", default= 'sequence.npy', help="E.g., node102,node103,node104 ")
+    return argparser.parse_args()
 
+# Test connection of nodes
 def check_ssh(server_ip, port=22):
     try:
         test_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -16,37 +21,31 @@ def check_ssh(server_ip, port=22):
         test_socket.close()
     return True
 
-def checkInput (string) :
-    nodes = args.workers.strip().split(',')
+# Turn command line arguments to list of nodes, then test connections to nodes.
+def check_node_input (arguments) :
+    if arguments is None:
+        raise ValueError('No workers specified.')
+    nodes = arguments.strip().split(',')
     for n in nodes :
         if not check_ssh(n) :
             raise ValueError(f"Can't connect to: {n}.")
     return nodes
-    
+
+# Split input file into different parts
 def splitInput (filename, workers) :
     path = os.getcwd()
     worker_count = len(workers)
     if worker_count == 0 :
         raise ValueError('Not enough workers.')
     sequence = np.load(os.path.join(path, filename))
-    return np.split(sequence,worker_count)
+    splits = np.split(sequence,worker_count)
+    return splits[0], splits[1:]
 
-# Parsing command line arguments
-argparser = argparse.ArgumentParser()
-argparser.add_argument("--workers", help="E.g., node102,node103,node104 ")
-argparser.add_argument("--input_file", default= 'sequence.npy', help="E.g., node102,node103,node104 ")
-args = argparser.parse_args()
+args = command_line_arguments()
+master, workers = check_node_input(args.nodes)
+file_splits = splitInput (args.input_file, workers)
 
-if args.workers is None:
-    raise ValueError('No workers specified.')
 
-nodes = checkInput(args.workers)
-master = nodes[0]
-workers = nodes[1:]
-
-splits = splitInput (args.input_file, workers)
-print(splits)
-print(f"Can connect to {master}: {check_ssh(master)}")
 # scp /path/to/file username@a:/path/to/destination
 
 
