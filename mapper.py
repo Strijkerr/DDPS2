faad72fc73b5c4c211f4024aac5834ea4d7a332a
Jonathan S <jonastrijker@gmail.com>
2022-12-13 23:27:46 +0100

    mapper function outlined

---
+++ mapper.py
@@ -0,0 +1,11 @@
+import numpy as np
+import collections
+import sys
+import pickle
+
+shard = sys.argv[1] # First argument
+test =  np.load(shard)
+count = collections.Counter(test)
+filename = shard.split('/')[-1].split('.')[0]
+with open(f'temp/{filename}.pickle', 'wb') as outputfile:
+    pickle.dump(count, outputfile)
