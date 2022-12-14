dec625e09342c5a5afd4168b408e81318c7b5827
Jonathan S <jonastrijker@gmail.com>
2022-12-14 15:17:25 +0100

    testtt

--- helloworld.py
+++ helloworld.py
@@ -2,7 +2,7 @@ import time
 import sys
 import json
 
-json_dictionary = sys.argv[1]
+json_dictionary = sys.argv[1:]
 #data = json.load(json_dictionary)
 print(json_dictionary)
 #print("Type:", data)
