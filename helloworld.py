8a1a9b08075c57a84a4803da427546985349860d
Jonathan S <jonastrijker@gmail.com>
2022-12-14 15:47:43 +0100

    sadasdads

--- helloworld.py
+++ helloworld.py
@@ -3,8 +3,9 @@ import sys
 import json
 
 json_dictionary = sys.argv[1:]
-json_dictionary = dict( pair.split('=') for pair in str(json_dictionary).split(',') )
+json_dictionary = ''.join(json_dictionary)
+#json_dictionary = dict( pair.split('=') for pair in str(json_dictionary).split(',') )
 print(json_dictionary)
-# json_dictionary = ''.join(json_dictionary)
+
 # data = json.loads(json_dictionary)
 # print("Type:", data)
