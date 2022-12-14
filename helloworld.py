e79bfa5826038fc667c907c12f36824c25ea5e2a
Jonathan S <jonastrijker@gmail.com>
2022-12-14 15:08:27 +0100

    test

--- helloworld.py
+++ helloworld.py
@@ -3,8 +3,5 @@ import sys
 import json
 
 json_dictionary = sys.argv[1]
-with open(json_dictionary) as json_file:
-    data = json.load(json_file)
- 
-    # Print the type of data variable
-    print("Type:", data)
+data = json.load(json_dictionary)
+print("Type:", data)
