e33dd600aa40b478d076f93e34583dc5c30c80b5
Jonathan S <jonastrijker@gmail.com>
2022-12-14 15:44:05 +0100

    adsasddsa

--- main.py
+++ main.py
@@ -138,7 +138,7 @@ json_dictionary = json.dumps(dictionary)
 
 # Fork process
 pid = os.fork()
-
+print("Main:",json_dictionary)
 # The parent process (master node)
 if pid > 0 :
     process = subprocess.Popen(f"ssh {master} python3 ~/DDPS2/helloworld.py {json_dictionary}", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
