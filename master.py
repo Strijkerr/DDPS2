ea3bb68649c111588dc595d36cf6aa709903d269
Jonathan S <jonastrijker@gmail.com>
2022-12-14 23:28:30 +0100

    testtt

--- master.py
+++ master.py
@@ -18,14 +18,14 @@ def server_program(clients):
     server_socket.listen(clients)
     conn, address = server_socket.accept()
     print("Connection from: " + str(address))
-    while True:
-        data = conn.recv(1024).decode()
-        if not data:
-            # if data is not received break
-            break
-        print("from connected user: " + str(data))
-        data = input(' -> ')
-        conn.send(data.encode())  # send data to the client
+    # while True:
+    #     data = conn.recv(1024).decode()
+    #     if not data:
+    #         # if data is not received break
+    #         break
+    #     print("from connected user: " + str(data))
+    #     data = input(' -> ')
+    #     conn.send(data.encode())  # send data to the client
 
     conn.close()  # close the connection
 
