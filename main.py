d97117ace1630a3d69c5466d4144ee6e9756d8d0
Jonathan S <jonastrijker@gmail.com>
2022-12-14 15:54:46 +0100

    saddsadsaads

--- main.py
+++ main.py
@@ -132,6 +132,7 @@ for index, file in enumerate(files):
 print("(Complete) Data has been split and distributed over cluster.")
 
 json_dictionary = json.dumps(dictionary)
+json_dictionary = str(json_dictionary).replace(" ","")
 print("Main:",json_dictionary)
 
 # Fork process
