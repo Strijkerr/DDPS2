86fff05b1fedcb9e2874ebb4113c2d70e7c54055
Jonathan S <jonastrijker@gmail.com>
2022-12-14 16:02:01 +0100

    sdaasddsadsa

--- helloworld.py
+++ helloworld.py
@@ -3,9 +3,9 @@ import sys
 import json
 import ast
 
-json_dictionary = sys.argv[1:]
-json_dictionary = ' '.join(json_dictionary)
-json_dictionary = json.loads(json_dictionary)
+json_dictionary = sys.argv[1]
+# json_dictionary = ' '.join(json_dictionary)
+# json_dictionary = json.loads(json_dictionary)
 print(json_dictionary)
 
 
