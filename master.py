314e6bc53d699bbe2e0eeec9a9f57dfb1bc6e05f
Jonathan S <jonastrijker@gmail.com>
2022-12-14 23:51:57 +0100

    multithreading test

--- master.py
+++ master.py
@@ -1,6 +1,7 @@
 import socket
 import sys
 import pickle
+import threading
 
 def returnDict (filename) :
     infile = open(filename,'rb')
@@ -8,25 +9,38 @@ def returnDict (filename) :
     infile.close()
     return dictionary
 
-    # https://www.digitalocean.com/community/tutorials/python-socket-programming-server-client
+# https://stackoverflow.com/questions/10810249/python-socket-multiple-clients
+def on_new_client(conn):
+    conn.send("Welcome to the Server. Type messages and press enter to send.\n")
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+        reply = "OK . . " + data
+        conn.sendall(reply)
+    conn.close()
+
+# https://www.digitalocean.com/community/tutorials/python-socket-programming-server-client
 def server_program(client_count):
     host = socket.gethostname()
     port = 56598
     server_socket = socket.socket()
     server_socket.bind((host, port)) 
     server_socket.listen(client_count)
-    conn, address = server_socket.accept()
-    print("Connection from: " + str(address))
+    
     while True:
-        data = conn.recv(1024).decode()
-        if not data:
-            # if data is not received break
-            break
-        print("from connected user: " + str(data))
-        data = input(' -> ')
-        conn.send(data.encode())  # send data to the client
+        conn, address = server_socket.accept()
+        print("Connection from: " + str(address))
+        threading.start_new_thread(on_new_client,(conn, ))
+        # data = conn.recv(1024).decode()
+        # if not data:
+        #     # if data is not received break
+        #     break
+        # print("from connected user: " + str(data))
+        # data = input(' -> ')
+        # conn.send(data.encode())  # send data to the client
 
-    conn.close()  # close the connection
+        #conn.close()  # close the connection
 
 shard_dict = returnDict(sys.argv[1])
 task_dict = returnDict(sys.argv[2])
