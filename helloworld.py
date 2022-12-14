1e07a081ab6394c033a16c86f5675a7752120961
Jonathan S <jonastrijker@gmail.com>
2022-12-14 15:42:14 +0100

    sadadsasddsa

--- helloworld.py
+++ helloworld.py
@@ -2,7 +2,7 @@ import time
 import sys
 import json
 
-json_dictionary = sys.argv[1]
+json_dictionary = sys.argv[1:]
 print(json_dictionary)
 # json_dictionary = ''.join(json_dictionary)
 data = json.loads(json_dictionary)
