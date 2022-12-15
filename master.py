bbc27906aaceb9f17a61bbbf49557bf0422f3658
Jonathan S <jonastrijker@gmail.com>
2022-12-15 16:58:48 +0100

    test

--- master.py
+++ master.py
@@ -67,12 +67,16 @@ def server_program(client_count):
         conn, address = server_socket.accept()
         list_of_clients.append(conn)
         t = threading.Thread(target=on_new_client, args=(conn,))
-        #t.daemon = True
+        t.daemon = True
         t.start()
     # At this point the daemons for every client have been created.
     print("All clients connected. Program exit")
     # while True :
     #     time.sleep(1)
+
+    # Force close
+    for client in list_of_clients :
+        client.close()
 
 shard_dict = returnDict(sys.argv[1])
 map_task_dict = returnDict(sys.argv[2])
