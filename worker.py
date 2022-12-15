5965fa7f85f7c2dcb4da51f35cc00b856a920d22
Jonathan S <jonastrijker@gmail.com>
2022-12-15 21:01:15 +0100

    sadadsdsa

--- worker.py
+++ worker.py
@@ -68,5 +68,5 @@ def client_program(master, worker):
             # If can't connect yet, wait 5 seconds and try again.
             time.sleep(5)
             continue
-        
+print("worker.py test")        
 client_program(sys.argv[1], sys.argv[2])
