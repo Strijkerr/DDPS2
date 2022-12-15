37b93791d501fdcf3de0ff21dcb42d93b6298132
Jonathan S <jonastrijker@gmail.com>
2022-12-15 17:17:21 +0100

    test

--- master.py
+++ master.py
@@ -75,6 +75,9 @@ def server_program(client_count):
     # At this point the daemons for every client have been created.
     print("All clients connected. Program exit")
 
+while True :
+    time.sleep(2)
+
 shard_dict = returnDict(sys.argv[1])
 map_task_dict = returnDict(sys.argv[2])
 reduce_task_dict = returnDict(sys.argv[3])
