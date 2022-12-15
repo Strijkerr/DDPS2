5f506fd870b65eebcdf9ce2886038e1de91dfa43
Jonathan S <jonastrijker@gmail.com>
2022-12-15 02:48:08 +0100

    test worker.py update

--- worker.py
+++ worker.py
@@ -1,22 +1,26 @@
 import socket
 import sys
+import time
 
-# https://www.digitalocean.com/community/tutorials/python-socket-programming-server-client
 def client_program(master):
-    print("test_worker.py")
-    host = master
-    port = 56609
+    while True :
+        host = master
+        port = 56609
 
-    client_socket = socket.socket()
-    client_socket.connect((host, port))
+        client_socket = socket.socket()
+        try :
+            client_socket.connect((host, port))
+            message = input(" -> ")  # take input
+            while message.lower().strip() != 'bye':
+                client_socket.send(message.encode())
+                data = client_socket.recv(1024).decode()
+                print('Received from server: ' + data)
+                message = input(" -> ")  # again take input
+        except :
+            print("FAILED. Sleep briefly & try again")
+            time.sleep(10)
+            continue
 
-    message = input(" -> ")  # take input
-    while message.lower().strip() != 'bye':
-        client_socket.send(message.encode())
-        data = client_socket.recv(1024).decode()
-        print('Received from server: ' + data)
-        message = input(" -> ")  # again take input
-
-    client_socket.close()
+        client_socket.close()
 
 client_program(sys.argv[1])
