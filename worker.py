e94ef40858c23ad6c944d98fb0266995d6df7422
Jonathan S <jonastrijker@gmail.com>
2022-12-15 02:05:58 +0100

    new port

--- worker.py
+++ worker.py
@@ -4,7 +4,7 @@ import sys
 # https://www.digitalocean.com/community/tutorials/python-socket-programming-server-client
 def client_program(master):
     host = master
-    port = 56608
+    port = 56609
 
     client_socket = socket.socket()
     client_socket.connect((host, port))
