7f8ea6b80cf36ea4cd56a0cfdf34980f7a190218
Jonathan S <jonastrijker@gmail.com>
2022-12-15 01:29:10 +0100

    dsfsdfsf

--- master.py
+++ master.py
@@ -32,15 +32,14 @@ def server_program(client_count):
     while True:
         conn, address = server_socket.accept()
         print("Connection from: " + str(address))
-        #start_new_thread(on_new_client,(conn, ))
         data = conn.recv(1024).decode()
         if not data:
-            # if data is not received break
             break
         print("from connected user: " + str(data))
         data = "ok"
-        conn.send(data.encode())  # send data to the client
-        conn.close()  # close the connection
+        conn.send(data.encode())
+        #start_new_thread(on_new_client,(conn, ))
+        conn.close()
 
 shard_dict = returnDict(sys.argv[1])
 task_dict = returnDict(sys.argv[2])
