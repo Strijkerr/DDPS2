003fe77df8ba72c14692222a5d5c8c5c20cbf450
Jonathan S <jonastrijker@gmail.com>
2022-12-15 01:42:41 +0100

    testt

--- worker.py
+++ worker.py
@@ -9,14 +9,14 @@ def client_program(master):
     client_socket = socket.socket()  # instantiate
     client_socket.connect((host, port))  # connect to the server
 
+    data = client_socket.recv(1024).decode()
+    print('Received from server: ' + data)
+
     message = input(" -> ")  # take input
-
     while message.lower().strip() != 'bye':
-        client_socket.send(message.encode())  # send message
-        data = client_socket.recv(1024).decode()  # receive response
-
-        print('Received from server: ' + data)  # show in terminal
-
+        client_socket.send(message.encode())
+        data = client_socket.recv(1024).decode()
+        print('Received from server: ' + data)
         message = input(" -> ")  # again take input
 
     client_socket.close()
