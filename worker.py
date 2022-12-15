6471c9ef00ed70845e7374bfd939af64d0744d19
Jonathan S <jonastrijker@gmail.com>
2022-12-15 01:20:21 +0100

    address was already in use

--- worker.py
+++ worker.py
@@ -4,7 +4,7 @@ import sys
 # https://www.digitalocean.com/community/tutorials/python-socket-programming-server-client
 def client_program(master):
     host = master
-    port = 56598 
+    port = 56599 
 
     client_socket = socket.socket()  # instantiate
     client_socket.connect((host, port))  # connect to the server
