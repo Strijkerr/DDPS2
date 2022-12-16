9fb675bd4a2ed659aaa1a42129df1c15ce2627dc
Jonathan S <jonastrijker@gmail.com>
2022-12-17 00:50:49 +0100

    testt

--- experiments.sh
+++ experiments.sh
@@ -2,6 +2,6 @@
 counter=0
 while [ $counter -le 9 ]
 do
-python3 create_data.py --seed $counter --sequence_length 1000000 &> filename.txt
+python3 create_data.py --seed $counter --sequence_length 1000000 | grep "time" &> filename.txt
 ((counter++))
 done
