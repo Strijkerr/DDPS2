70e04881cdb2fb133d8226785d03553855abaa8e
Jonathan S <jonastrijker@gmail.com>
2022-12-17 16:52:55 +0100

    asdsadasd

--- experiment_ddps_splits.sh
+++ experiment_ddps_splits.sh
@@ -18,7 +18,7 @@ python3 create_data.py --seed $counter --sequence_length 10000000 --measure_perf
 #python3 main.py --nodes $node_list --input sequence.npy --partitions 1 --splits 5 --copies 2 #| grep -s "time" | cut -d ":" -f 2 | cut -d " " -f 2 &>> Times/ddps_partitions_1_splits_5_copies_2.txt
 
 # Different partitions
-python3 main.py --nodes $node_list --input sequence.npy --partitions 1 --splits 5 --copies 1 #| grep -s "time" | cut -d ":" -f 2 | cut -d " " -f 2 &>> Times/ddps_partitions_2_splits_5_copies_1.txt
+python3 main.py --nodes $node_list --input sequence.npy --partitions 2 --splits 5 --copies 1 #| grep -s "time" | cut -d ":" -f 2 | cut -d " " -f 2 &>> Times/ddps_partitions_2_splits_5_copies_1.txt
 python3 main.py --nodes $node_list --input sequence.npy --partitions 3 --splits 5 --copies 1 #| grep -s "time" | cut -d ":" -f 2 | cut -d " " -f 2 &>> Times/ddps_partitions_3_splits_5_copies_1.txt
 ((counter++))
 done
