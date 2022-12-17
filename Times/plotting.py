9966f1cd79cf13520d35e08c9595b8db523fc8aa
Jonathan S <jonastrijker@gmail.com>
2022-12-17 02:01:25 +0100

    testtt

--- Times/plotting.py
+++ Times/plotting.py
@@ -3,7 +3,7 @@ sequences = [1000000, 10000000, 100000000]
 for s in sequences :
     f = open(f"single_system_{s}.txt", "r")
     total = 0
-    for index,x in enumerate(f):
+    for x in f:
         total+=float(x)
-        print(index)
+        print(len(f))
     print(total)
