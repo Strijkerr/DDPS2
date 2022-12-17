3a0bbbac518aa686fc63922f389dfabacbb10c8e
Jonathan S <jonastrijker@gmail.com>
2022-12-17 18:41:03 +0100

    sdaasdadsads

--- experiment_ddps_splits.sh
+++ experiment_ddps_splits.sh
@@ -13,7 +13,7 @@ read -ra node_list <<< "$nodes"; unset IFS
 counter=0
 while [ $counter -le 9 ]
 do
-python3 create_data.py --seed $counter --sequence_length 100000000 --measure_performance False > /dev/null
+python3 create_data.py --seed $counter --sequence_length 10000000 --measure_performance False > /dev/null
 
 # Different copies
 python3 main.py --nodes $node_list --input sequence.npy --partitions 2 --splits 5 --copies 1 #| grep -s "time" | cut -d ":" -f 2 | cut -d " " -f 2 &>> Times/ddps_partitions_1_splits_5_copies_1.txt
