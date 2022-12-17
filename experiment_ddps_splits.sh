372509da8f1d7b5f8b3b430a46cadc31c232e14b
Jonathan S <jonastrijker@gmail.com>
2022-12-17 03:06:16 +0100

    testt

--- experiment_ddps_splits.sh
+++ experiment_ddps_splits.sh
@@ -6,6 +6,6 @@ counter=0
 while [ $counter -le 9 ]
 do
 python3 create_data.py --seed $counter --sequence_length 100000000 --measure_performance False > /dev/null
-python3 main.py --nodes $node_list --input sequence.npy --partitions 1 --splits 5 --copies 1 | grep "time" | cut -d ":" -f 2 | cut -d " " -f 2 2> /dev/null
+python3 main.py --nodes $node_list --input sequence.npy --partitions 1 --splits 5 --copies 1 | grep -s "time" | cut -d ":" -f 2 | cut -d " " -f 2
 ((counter++))
 done
