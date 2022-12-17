dad74ac8b598d36956ee49d4a1a11c44e480756f
Jonathan S <jonastrijker@gmail.com>
2022-12-17 01:13:44 +0100

    dsadsadasdsa

--- experiments.sh
+++ experiments.sh
@@ -2,9 +2,9 @@
 counter=0
 local -i int
 int=1000000
-> single_system_"$length".txt
+> single_system_"$int".txt
 while [ $counter -le 9 ]
 do
-python3 create_data.py --seed $counter --sequence_length "$int" | grep "time" | cut -d ":" -f 2 | cut -d " " -f 2 &>> single_system_"$length".txt
+python3 create_data.py --seed $counter --sequence_length "$int" | grep "time" | cut -d ":" -f 2 | cut -d " " -f 2 &>> single_system_"$int".txt
 ((counter++))
 done
