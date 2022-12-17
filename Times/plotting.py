19dade3004d8e656e2c669775c84768ed210a2e3
Jonathan S <jonastrijker@gmail.com>
2022-12-17 01:50:01 +0100

    sadasads

--- Times/plotting.py
+++ Times/plotting.py
@@ -1,8 +1,7 @@
-
-sequences = [1000000, 10000000, 100000000, 1000000000]
+sequences = [1000000, 10000000, 100000000]
 
 for s in sequences :
-    f = open(f"Times/single_system_{s}.txt", "r")
+    f = open(f"single_system_{s}.txt", "r")
     total = 0
     for index,x in enumerate(f):
         total+=float(x)
