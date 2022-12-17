fe5f916fb1d067490cf0beb96270ee5e8d31668b
Jonathan S <jonastrijker@gmail.com>
2022-12-17 04:52:20 +0100

    bugfix

--- main.py
+++ main.py
@@ -219,7 +219,7 @@ pid = os.fork()
 
 # Start master node.
 if pid > 0 :
-    process = subprocess.Popen(f"ssh {master} python3 ~/DDPS2/master.py {location1} {location2} {location3} {location4}", shell=True, stdout=sys.stdout, stderr=sys.stderr, bufsize=1)
+    process = subprocess.Popen(f"ssh {master} python3 ~/DDPS2/master.py {location1} {location2} {location3} {location4}", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=1)
     stdout, stder = process.communicate() # Blocking
     print('\n#############################################################')
     print("Stdout:",stdout.decode('ASCII'))
