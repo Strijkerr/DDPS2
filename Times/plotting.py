94e0c6b4dfb5c4677024be056ae2ebcc32c72fd0
Jonathan S <jonastrijker@gmail.com>
2022-12-17 02:02:13 +0100

    testtt

--- Times/plotting.py
+++ Times/plotting.py
@@ -3,7 +3,9 @@ sequences = [1000000, 10000000, 100000000]
 for s in sequences :
     f = open(f"single_system_{s}.txt", "r")
     total = 0
+    count = 0
     for x in f:
         total+=float(x)
-        print(len(f))
-    print(total)
+        count+=1
+        print(x)
+    print("Final average:",total/len(count))
