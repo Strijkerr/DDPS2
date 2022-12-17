ce0f3ef29cf261078f8c027200c736dbfc4e4129
Jonathan S <jonastrijker@gmail.com>
2022-12-17 16:02:49 +0100

    debugging

--- main.py
+++ main.py
@@ -249,7 +249,7 @@ else :
     for worker in workers:
         pid = os.fork()
         if pid:
-            #process = subprocess.Popen(f"ssh {worker} python3 ~/DDPS2/worker.py {master} {worker}", shell=True, stdout=sys.stdout, stderr=sys.stderr) # debugging client side
-            process = subprocess.Popen(f"ssh {worker} python3 ~/DDPS2/worker.py {master} {worker}", shell=True)
+            process = subprocess.Popen(f"ssh {worker} python3 ~/DDPS2/worker.py {master} {worker}", shell=True, stdout=sys.stdout, stderr=sys.stderr) # debugging client side
+            #process = subprocess.Popen(f"ssh {worker} python3 ~/DDPS2/worker.py {master} {worker}", shell=True)
         else:
             os._exit(0)
