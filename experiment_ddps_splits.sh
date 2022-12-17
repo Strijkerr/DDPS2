2c4089914b920c076565016a797e558f3d8283ce
Jonathan S <jonastrijker@gmail.com>
2022-12-17 05:13:57 +0100

    testtt

--- experiment_ddps_splits.sh
+++ experiment_ddps_splits.sh
@@ -2,8 +2,8 @@
 nodes=${1}
 read -ra node_list <<< "$nodes"; unset IFS
 # Different copies
-> Times/ddps_partitions_1_splits_5_copies_1.txt
-> Times/ddps_partitions_1_splits_5_copies_2.txt
+#> Times/ddps_partitions_1_splits_5_copies_1.txt
+#> Times/ddps_partitions_1_splits_5_copies_2.txt
 
 # Different partitions
 > Times/ddps_partitions_2_splits_5_copies_1.txt
@@ -14,8 +14,8 @@ do
 python3 create_data.py --seed $counter --sequence_length 10000000 --measure_performance False > /dev/null
 
 # Different copies
-python3 main.py --nodes $node_list --input sequence.npy --partitions 1 --splits 5 --copies 1 #| grep -s "time" | cut -d ":" -f 2 | cut -d " " -f 2 &>> Times/ddps_partitions_1_splits_5_copies_1.txt
-python3 main.py --nodes $node_list --input sequence.npy --partitions 1 --splits 5 --copies 2 #| grep -s "time" | cut -d ":" -f 2 | cut -d " " -f 2 &>> Times/ddps_partitions_1_splits_5_copies_2.txt
+#python3 main.py --nodes $node_list --input sequence.npy --partitions 1 --splits 5 --copies 1 #| grep -s "time" | cut -d ":" -f 2 | cut -d " " -f 2 &>> Times/ddps_partitions_1_splits_5_copies_1.txt
+#python3 main.py --nodes $node_list --input sequence.npy --partitions 1 --splits 5 --copies 2 #| grep -s "time" | cut -d ":" -f 2 | cut -d " " -f 2 &>> Times/ddps_partitions_1_splits_5_copies_2.txt
 
 # Different partitions
 python3 main.py --nodes $node_list --input sequence.npy --partitions 2 --splits 5 --copies 1 #| grep -s "time" | cut -d ":" -f 2 | cut -d " " -f 2 &>> Times/ddps_partitions_2_splits_5_copies_1.txt
@@ -24,8 +24,8 @@ python3 main.py --nodes $node_list --input sequence.npy --partitions 3 --splits 
 done
 
 # # Different copies
-python3 Times/experiments_ddps.py "Times/ddps_partitions_1_splits_5_copies_1.txt"
-python3 Times/experiments_ddps.py "Times/ddps_partitions_1_splits_5_copies_2.txt"
+#python3 Times/experiments_ddps.py "Times/ddps_partitions_1_splits_5_copies_1.txt"
+#python3 Times/experiments_ddps.py "Times/ddps_partitions_1_splits_5_copies_2.txt"
 
 # Different partitions
 python3 Times/experiments_ddps.py "Times/ddps_partitions_2_splits_5_copies_1.txt"
