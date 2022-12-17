125f84a93905a5bde5c32a82ea826d6bdbfb1b4b
Jonathan S <jonastrijker@gmail.com>
2022-12-17 20:01:03 +0100

    running splits and partition experiments

--- experiments_ddps.sh
+++ experiments_ddps.sh
@@ -3,12 +3,12 @@ nodes=${1}
 read -ra node_list <<< "$nodes"; unset IFS
 
 # Base
-> Times/ddps_partitions_1_splits_5_copies_1.txt
+#> Times/ddps_partitions_1_splits_5_copies_1.txt
 
 # Different partitions & splits
-# > Times/ddps_partitions_1_splits_10_copies_1.txt
-# > Times/ddps_partitions_2_splits_5_copies_1.txt
-# > Times/ddps_partitions_2_splits_10_copies_1.txt
+> Times/ddps_partitions_1_splits_10_copies_1.txt
+> Times/ddps_partitions_2_splits_5_copies_1.txt
+> Times/ddps_partitions_2_splits_10_copies_1.txt
 
 # Different copies
 # > Times/ddps_partitions_1_splits_5_copies_2.txt
@@ -22,12 +22,12 @@ do
 python3 create_data.py --seed $counter --sequence_length 100000000 --measure_performance False > /dev/null # Change to 10^8 after
 
 # Base
-python3 main.py --nodes $node_list --input sequence.npy --partitions 1 --splits 5 --copies 1 | grep -s "time" | cut -d ":" -f 2 | cut -d " " -f 2 &>> Times/ddps_partitions_1_splits_5_copies_1.txt
+#python3 main.py --nodes $node_list --input sequence.npy --partitions 1 --splits 5 --copies 1 | grep -s "time" | cut -d ":" -f 2 | cut -d " " -f 2 &>> Times/ddps_partitions_1_splits_5_copies_1.txt
 
 # Different partitions & splits
-#python3 main.py --nodes $node_list --input sequence.npy --partitions 1 --splits 10 --copies 1 | grep -s "time" | cut -d ":" -f 2 | cut -d " " -f 2 &>> Times/ddps_partitions_1_splits_10_copies_1.txt
-#python3 main.py --nodes $node_list --input sequence.npy --partitions 2 --splits 5 --copies 1 | grep -s "time" | cut -d ":" -f 2 | cut -d " " -f 2 &>> Times/ddps_partitions_2_splits_5_copies_1.txt
-#python3 main.py --nodes $node_list --input sequence.npy --partitions 2 --splits 10 --copies 1 | grep -s "time" | cut -d ":" -f 2 | cut -d " " -f 2 &>> Times/ddps_partitions_2_splits_10_copies_1.txt
+python3 main.py --nodes $node_list --input sequence.npy --partitions 1 --splits 10 --copies 1 | grep -s "time" | cut -d ":" -f 2 | cut -d " " -f 2 &>> Times/ddps_partitions_1_splits_10_copies_1.txt
+python3 main.py --nodes $node_list --input sequence.npy --partitions 2 --splits 5 --copies 1 | grep -s "time" | cut -d ":" -f 2 | cut -d " " -f 2 &>> Times/ddps_partitions_2_splits_5_copies_1.txt
+python3 main.py --nodes $node_list --input sequence.npy --partitions 2 --splits 10 --copies 1 | grep -s "time" | cut -d ":" -f 2 | cut -d " " -f 2 &>> Times/ddps_partitions_2_splits_10_copies_1.txt
 
 # Different copies
 #python3 main.py --nodes $node_list --input sequence.npy --partitions 1 --splits 5 --copies 2 | grep -s "time" | cut -d ":" -f 2 | cut -d " " -f 2 &>> Times/ddps_partitions_1_splits_5_copies_2.txt
@@ -38,12 +38,12 @@ python3 main.py --nodes $node_list --input sequence.npy --partitions 1 --splits 
 done
 
 # Different copies
-python3 Times/experiments_ddps.py "Times/ddps_partitions_1_splits_5_copies_1.txt"
+#python3 Times/experiments_ddps.py "Times/ddps_partitions_1_splits_5_copies_1.txt"
 
 # Different partitions
-#python3 Times/experiments_ddps.py "Times/ddps_partitions_1_splits_10_copies_1.txt"
-#python3 Times/experiments_ddps.py "Times/ddps_partitions_2_splits_5_copies_1.txt"
-#python3 Times/experiments_ddps.py "Times/ddps_partitions_2_splits_10_copies_1.txt"
+python3 Times/experiments_ddps.py "Times/ddps_partitions_1_splits_10_copies_1.txt"
+python3 Times/experiments_ddps.py "Times/ddps_partitions_2_splits_5_copies_1.txt"
+python3 Times/experiments_ddps.py "Times/ddps_partitions_2_splits_10_copies_1.txt"
 
 # Different copies
 #python3 Times/experiments_ddps.py "Times/ddps_partitions_1_splits_5_copies_2.txt"
