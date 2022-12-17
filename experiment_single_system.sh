61c4dfb0ef37f0818de0df6ca5997305af6f4a10
Jonathan S <jonastrijker@gmail.com>
2022-12-17 01:52:28 +0100

    experiment

--- experiment_single_system.sh
+++ experiment_single_system.sh
@@ -3,10 +3,12 @@ counter=0
 > Times/single_system_1000000.txt
 > Times/single_system_10000000.txt
 > Times/single_system_100000000.txt
+> Times/single_system_1000000000.txt
 while [ $counter -le 9 ]
 do
 python3 create_data.py --seed $counter --sequence_length 1000000 | grep "time" | cut -d ":" -f 2 | cut -d " " -f 2 &>> Times/single_system_1000000.txt
 python3 create_data.py --seed $counter --sequence_length 10000000 | grep "time" | cut -d ":" -f 2 | cut -d " " -f 2 &>> Times/single_system_10000000.txt
 python3 create_data.py --seed $counter --sequence_length 100000000 | grep "time" | cut -d ":" -f 2 | cut -d " " -f 2 &>> Times/single_system_100000000.txt
+python3 create_data.py --seed $counter --sequence_length 1000000000 | grep "time" | cut -d ":" -f 2 | cut -d " " -f 2 &>> Times/single_system_1000000000.txt
 ((counter++))
 done
